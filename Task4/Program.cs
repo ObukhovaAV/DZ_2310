29770e803792c9b6e164f073d9910150b4e35c9c
Anastasya <okayota83@mail.ru>
2022-10-29 21:59:38 +0400

    changed Task4

--- Task4/Program.cs
+++ Task4/Program.cs
@@ -2,29 +2,41 @@
 int Factorial (int n)
 {
     int result = 1;
-    for (int i = 1; i <= n; i++)       
-        result*=i; 
-       
+    for (int i = 1; i <= n; i++)
+        result *= i;
+
     return result;
+}
+
+void FillArray(int[,] array, int N)
+{
+    array[0, 0] = 1;
+    for (int i = 1; i < array.GetLength(0); i++)
+
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = Factorial(i) / (Factorial(j) * Factorial(i - j));
+}
+void PrintArray (int [,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < (array.GetLength(1) - i); j++)
+            Console.Write(" ");
+        for (int k = 0; k < (array.GetLength(0)); k++)
+        {
+            if (array[i, k] == 0)
+                Console.Write(" ");
+            else
+                Console.Write($" {array[i, k]}");
+        }
+        Console.WriteLine();
+    }
 }
 
 
 Console.WriteLine("Введите количество строк треугольника Паскаля");
 int N = int.Parse(Console.ReadLine()?? "0");
-//int n = 0;
-for (int n = 0; n < N; n++)
-{
-    for (int i = 0; i <= N - n; i++)
-
-    {
-        Console.Write(" ");
-    }
-    for (int j = 0; j <= n; j++)
-    {
-        Console.Write(" ");
-        Console.Write(Factorial(n) / (Factorial(j) * Factorial(n - j)));
-       
-    }
+int [,] array = new int [N,N];
+FillArray(array, N);
 Console.WriteLine();
-
-}
+PrintArray (array);
